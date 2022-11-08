c4ff48d666db793d821e2224930f4052440007c8
Akshay Nataraja <akshaynataraja@Akshays-MacBook-Pro.local>
2022-11-07 21:37:36 -0500

    created javascript page and got a domain

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+$(document).ready(function() {
+//All of jquery code
+
+//cool stuff
+// mouseenter,hover or mouseleave - mouse hovers
+//dbl - double click
+//fadeToggle(speed) - faded
+
+    $("button").click( function() {
+        $("#p").toggle();   // the hashtag lets you use with an id and dots for classes and square bracks for elenebts like href
+        let div = $("div");
+        //div.animate({},)
+    }
+    );
+
+}
+);
+
+
 document.getElementById('demo').innerHTML='new text';
-document.getElementById('demo').id = "demo2"
-document.getElementById('demo2').style.color = "red";
-document.getElementById('demo2').outerHTML = "<button> something to press </button>"
+// document.getElementById('demo').id = "demo2"
+document.getElementById('demo').style.color = "red";
+// document.getElementById('demo').outerHTML = "<button> something to press </button>"
 
 
 function myFun(){
     alert("WARNING")
 }
-myFun();
+
 
 function darkMode(){
     document.getElementById("pagestyle").setAttribute("href", "./styleDark.css")
@@ -20,3 +39,21 @@ const c = 30; //cant be changed
 var a = 20;
 
 console.log(a+b+c)
+
+function key_pressed(input) {
+    console.log(input);
+    document.getElementById("keypressed_text").innerHTML = input;
+}
+
+const key = function keyEvent(e) {
+
+    key_pressed();
+    if(e.key === "f"){
+        alert("f key pressed");
+
+    }
+
+
+}
+
+document.addEventListener("keypress", key);
